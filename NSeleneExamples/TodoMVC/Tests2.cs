d6ad752ea6faf4cc6f1e5a258d7c7059a50e0e18
Iakiv Kramarenko <yashaka@gmail.com>
2016-05-11 01:46:50 +0300

    added temp tests to examples project

---
+++ NSeleneExamples/TodoMVC/Tests2.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using static NSelene.Utils;
+using NSeleneExamples.TodoMVC.Pages;
+
+
+namespace NSeleneExamples
+{
+    namespace TodoMVC
+    {
+        [TestFixture]
+        [Parallelizable(ParallelScope.Fixtures)]
+        public class Test2 : BaseTest
+        {
+            [Test]
+            public void FilterTasks()
+            {
+                Tasks.Visit();
+
+                Tasks.Add("a", "b", "c");
+                Tasks.ShouldBe("a", "b", "c");
+
+                Tasks.Toggle("b"); 
+
+                Tasks.FilterActive();
+                Tasks.ShouldBe("a", "c");
+
+                Tasks.FilterCompleted();
+                Tasks.ShouldBe("b");
+            }
+        }
+    }
+
+}
---
+++ NSeleneExamples/TodoMVC/Tests4.cs
@@ -0,0 +1,70 @@
+﻿using NUnit.Framework;
+using static NSelene.Utils;
+using NSeleneExamples.TodoMVC.Pages;
+
+
+namespace NSeleneExamples
+{
+    namespace TodoMVC
+    {
+        [TestFixture]
+        [Parallelizable(ParallelScope.Self)]
+        public class Test4 : BaseTest
+        {
+            [Test]
+            [Parallelizable(ParallelScope.Self)]
+            public void FilterTasks()
+            {
+                Tasks.Visit();
+
+                Tasks.Add("a", "b", "c");
+                Tasks.ShouldBe("a", "b", "c");
+
+                Tasks.Toggle("b"); 
+
+                Tasks.FilterActive();
+                Tasks.ShouldBe("a", "c");
+
+                Tasks.FilterCompleted();
+                Tasks.ShouldBe("b");
+            }
+
+            [Test]
+            [Parallelizable(ParallelScope.Self)]
+            public void FilterTasks2()
+            {
+                Tasks.Visit();
+
+                Tasks.Add("a", "b", "c");
+                Tasks.ShouldBe("a", "b", "c");
+
+                Tasks.Toggle("b"); 
+
+                Tasks.FilterActive();
+                Tasks.ShouldBe("a", "c");
+
+                Tasks.FilterCompleted();
+                Tasks.ShouldBe("b");
+            }
+
+            [Test]
+            [Parallelizable(ParallelScope.Self)]
+            public void FilterTasks3()
+            {
+                Tasks.Visit();
+
+                Tasks.Add("a", "b", "c");
+                Tasks.ShouldBe("a", "b", "c");
+
+                Tasks.Toggle("b"); 
+
+                Tasks.FilterActive();
+                Tasks.ShouldBe("a", "c");
+
+                Tasks.FilterCompleted();
+                Tasks.ShouldBe("b");
+            }
+        }
+    }
+
+}
