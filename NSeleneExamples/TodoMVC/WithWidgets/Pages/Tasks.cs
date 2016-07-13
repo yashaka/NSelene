03debe09bf2fe8ee2cba588d33e34d9a974b1d55
Iakiv Kramarenko <yashaka@gmail.com>
2016-07-13 19:08:38 +0300

    added examples of custom elements/widgets/elementobjects

---
+++ NSeleneExamples/TodoMVC/WithWidgets/Pages/Tasks.cs
@@ -0,0 +1,63 @@
+using System;
+using OpenQA.Selenium;
+using NSelene;
+using static NSelene.Utils;
+
+namespace NSeleneExamples.TodoMVC.WithWidgets.Pages
+{
+    public class Task
+    {
+        SElement element;
+
+        public Task(SElement element)
+        {
+            this.element = element;
+        }
+
+        public Task Toggle()
+        {
+            this.element.Find(".toggle").Click();
+            return this;
+        }
+    }
+
+    public static class Tasks
+    {
+        public static SCollection List = SS("#todo-list>li");
+
+        public static void Visit()
+        {
+            Open("https://todomvc4tasj.herokuapp.com/");
+        }
+
+        public static void FilterActive()
+        {
+            S(By.LinkText("Active")).Click();
+        }
+
+        public static void FilterCompleted()
+        {
+            S(By.LinkText("Completed")).Click();
+        }
+
+        public static void Add(params string[] taskTexts)
+        {
+            foreach (var text in taskTexts)
+            {
+                S("#new-todo").Should(Be.Enabled).SetValue(text).PressEnter();
+            }
+        }
+
+        public static void Toggle(string taskText)
+        {
+            new Task(List.FindBy(Have.ExactText(taskText))).Toggle();
+            //List.FindBy(Have.ExactText(taskText)).Find(".toggle").Click();
+        }
+
+        public static void ShouldBe(params string[] names)
+        {
+            List.FilterBy(Be.Visible).Should(Have.Texts(names));
+        }
+    }
+
+}
---
+++ NSeleneExamples/TodoMVC/WithWidgets/TodoMVCTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using static NSelene.Utils;
+using NSeleneExamples.TodoMVC.WithWidgets.Pages;
+
+
+namespace NSeleneExamples.TodoMVC.WithWidgets
+{
+    [TestFixture]
+    public class TestTodoMVC : BaseTest
+    {
+        [Test]
+        public void FilterTasks()
+        {
+            Tasks.Visit();
+
+            Tasks.Add("a", "b", "c");
+            Tasks.ShouldBe("a", "b", "c");
+
+            Tasks.Toggle("b"); 
+
+            Tasks.FilterActive();
+            Tasks.ShouldBe("a", "c");
+
+            Tasks.FilterCompleted();
+            Tasks.ShouldBe("b");
+        }
+    }
+}
