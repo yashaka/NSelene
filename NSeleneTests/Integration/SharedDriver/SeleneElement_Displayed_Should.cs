8c6c128e15b0dd3a077a29a729b6fb28b6e2e054
yashaka <yashaka@gmail.com>
2020-07-25 14:20:44 +0300

    added SeleneElement_Displayed_Should.cs

---
+++ NSeleneTests/Integration/SharedDriver/SeleneElement_Displayed_Should.cs
@@ -0,0 +1,91 @@
+using NUnit.Framework;
+using static NSelene.Selene;
+using OpenQA.Selenium;
+
+namespace NSelene.Tests.Integration.SharedDriver.SeleneElementSpec
+{
+    using Harness;
+
+    [TestFixture]
+    public class SeleneElement_Displayed_Should : BaseTest
+    {
+
+        [TearDown]
+        public void TeardownTest()
+        {
+            Configuration.Timeout = 4;
+        }
+        
+        [Test]
+        public void NotStartActualSearch()
+        {
+            Given.OpenedEmptyPage();
+            var nonExistentElement = S("#not-existing-element-id");
+            Assert.IsNotEmpty(nonExistentElement.ToString()); 
+        }
+
+        [Test]
+        public void PostponeTheSearchUntilActualActionLikeQuestioiningDisplayed()
+        {
+            Given.OpenedEmptyPage();
+            var element = S("#will-be-existing-element-id");
+            When.WithBody(@"<h1 id='will-be-existing-element-id'>Hello kitty:*</h1>");
+            Assert.IsTrue(element.Displayed);
+        }
+
+        [Test]
+        public void UpdateSearchResults_OnNextActualActionLikeQuestioiningDisplayed()
+        {
+            Given.OpenedEmptyPage();
+            var element = S("#will-be-existing-element-id");
+            When.WithBody(@"<h1 id='will-be-existing-element-id'>Hello kitty:*</h1>");
+            Assert.IsTrue(element.Displayed);
+            When.WithBody(@"<h1 id='will-be-existing-element-id' style='display:none'>Hello kitty:*</h1>");
+            Assert.IsFalse(element.Displayed);
+        }
+
+        [Test]
+        public void WaitForVisibility_OnActionsLikeClick()
+        {
+            Given.OpenedPageWithBody(@"
+                <a href='#second' style='display:none'>go to Heading 2</a>
+                <h2 id='second'>Heading 2</h2>"
+            );
+            Selene.ExecuteScript(@"
+                setTimeout(
+                    function(){
+                        document.getElementsByTagName('a')[0].style = 'display:block';
+                    }, 
+                    500);"
+            );
+            S("a").Click();
+            Assert.IsTrue(Selene.GetWebDriver().Url.Contains("second"));
+        }
+
+        [Test]
+        public void FailWithTimeout_DuringWaitingForVisibilityOnActionsLikeClick()
+        {
+            Configuration.Timeout = 0.25;
+            Given.OpenedPageWithBody(@"
+                <a href='#second' style='display:none'>go to Heading 2</a>
+                <h2 id='second'>Heading 2</h2>"
+            );
+            Selene.ExecuteScript(@"
+                setTimeout(
+                    function(){
+                        document.getElementsByTagName('a')[0].style = 'display:block';
+                    }, 
+                    500);"
+            );
+
+            // TODO: consider using Assert.Throws<WebDriverTimeoutException>(() => { ... })
+            try {
+                S("a").Click();
+                Assert.Fail("should fail on timeout before can be clicked");
+            } catch (WebDriverTimeoutException) {
+                Assert.IsFalse(Selene.GetWebDriver().Url.Contains("second"));
+            }
+        }
+    }
+}
+
