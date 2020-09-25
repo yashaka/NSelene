2fb2635fe63849affd99683d1afd428a49b8cbf4
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2020-09-26 01:17:54 +0300

    add new test for Linq methods

--- NSeleneTests/Integration/SharedDriver/SeleneCollection_Enumerable_Should.cs
+++ NSeleneTests/Integration/SharedDriver/SeleneCollection_Enumerable_Should.cs
@@ -3,6 +3,7 @@ using NSelene.Tests.Integration.SharedDriver.Harness;
 using static NSelene.Selene;
 using NUnit.Framework;
 using System.Linq;
+using System;
 
 namespace NSeleneTests.Integration.SharedDriver.SeleneCollectionSpec
 {
@@ -17,24 +18,144 @@ namespace NSeleneTests.Integration.SharedDriver.SeleneCollectionSpec
         }
 
         [Test]
-        public void SeleneCollection_Enumerable_ShouldReturnFirst()
+        public void ReturnFirst()
         {
-            Given.OpenedEmptyPage();
-            SeleneCollection elements = SS("#will-exist>input");
-
-            When.WithBody(@"
+            Given.OpenedPageWithBody(@"
                 <p id='will-exist'>
                     <input id='ask' type='submit' value='How r u?' style='display:none'></input>
                     <input id='answer1' type='submit' value='Good!'></input>
                     <input id='answer2' type='submit' value='Great!'></input>
                 </p>"
             );
+
+            SeleneCollection elements = SS("#will-exist>input");
+
             var element = elements.ActualWebElements.First();
 
-            Assert.IsFalse(element.Displayed);
             Assert.AreEqual("input", element.TagName);
-            Assert.IsEmpty(element.Text);
-            Assert.AreEqual("ask", element.GetAttribute("id"));
+        }
+
+        [Test]
+        public void ReturnDefaultFirst()
+        {
+            Given.OpenedPageWithBody(@"
+                <p id='will-exist'>
+                    <input id='ask' type='submit' value='How r u?' style='display:none'></input>
+                    <input id='answer1' type='submit' value='Good!'></input>
+                    <input id='answer2' type='submit' value='Great!'></input>
+                </p>"
+            );
+
+            SeleneCollection elements = SS("div");
+
+            var element = elements.ActualWebElements.FirstOrDefault();
+
+            Assert.IsNull(element);
+        }
+
+        [Test]
+        public void ReturnLast()
+        {
+            Given.OpenedPageWithBody(@"
+                <ul>
+                    <li class='item'>a</li>
+                    <li class='item'>b</li>
+                    <li class='item'>c</li>
+                </ul>"
+            );
+
+            SeleneCollection elements = SS(".item");
+
+            var element = elements.ActualWebElements.Last();
+
+            Assert.AreEqual("c", element.Text);
+        }
+
+        [Test]
+        public void ReturnDefaultLast()
+        {
+            Given.OpenedPageWithBody(@"
+                <ul>
+                    <li class='item'>a</li>
+                    <li class='item'>b</li>
+                    <li class='item'>c</li>
+                </ul>"
+            );
+
+            SeleneCollection elements = SS("div");
+
+            var element = elements.ActualWebElements.LastOrDefault();
+
+            Assert.IsNull(element);
+        }
+
+        [Test]
+        public void ReturnSingle()
+        {
+            Given.OpenedPageWithBody(@"
+                <ul>
+                    <li class='item'>a</li>
+                    <li class='item'>b</li>
+                    <li class='item'>c</li>
+                </ul>"
+            );
+
+            SeleneCollection elements = SS(".item");
+
+            var element = elements.ActualWebElements.Single(e => e.Text == "c");
+
+            Assert.AreEqual("c", element.Text);
+        }
+
+        [Test]
+        public void ReturnSingleThrowException()
+        {
+            Given.OpenedPageWithBody(@"
+                <ul>
+                    <li class='item'>c</li>
+                    <li class='item'>c</li>
+                </ul>"
+            );
+
+            SeleneCollection elements = SS(".item");
+
+            Assert.Throws<InvalidOperationException>(() => elements.ActualWebElements.Single(e => e.Text == "c"));
+        }
+
+        [Test]
+        public void ReturnDefaultSingle()
+        {
+            Given.OpenedPageWithBody(@"
+                <ul>
+                    <li class='item'>a</li>
+                    <li class='item'>a</li>
+                    <li class='item'>a</li>
+                </ul>"
+            );
+
+            SeleneCollection elements = SS(".item");
+
+            var element = elements.ActualWebElements.SingleOrDefault(e => e.Text == "b");
+
+            Assert.IsNull(element);
+        }
+
+        [Test]
+        public void ReturnSelect()
+        {
+            Given.OpenedPageWithBody(@"
+                <ul>
+                    <li class='item'>a</li>
+                    <li class='item'>b</li>
+                </ul>"
+            );
+
+            SeleneCollection elements = SS(".item");
+
+            var selectedElements = elements.ActualWebElements.Select(e => e.Text == "b");
+
+            Assert.AreEqual(false, selectedElements.First());
+            Assert.AreEqual(true, selectedElements.Last());
         }
     }
 }
