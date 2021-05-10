a7b594d2e6610fb5ff5a819c56b8260783d95b51
Iakiv Kramarenko <yashaka@gmail.com>
2021-05-10 22:07:27 +0300

    added time checks to click tests

--- NSeleneTests/Integration/SharedDriver/SeleneElement_Click_Specs.cs
+++ NSeleneTests/Integration/SharedDriver/SeleneElement_Click_Specs.cs
@@ -3,6 +3,8 @@ using static NSelene.Selene;
 
 namespace NSelene.Tests.Integration.SharedDriver.SeleneSpec
 {
+    using System;
+    using System.Linq;
     using Harness;
 
     [TestFixture]
@@ -16,6 +18,7 @@ namespace NSelene.Tests.Integration.SharedDriver.SeleneSpec
             Configuration.Timeout = 0.6;
             Configuration.PollDuringWaits = 0.1;
             Given.OpenedEmptyPage();
+            var beforeCall = DateTime.Now;
             Given.OpenedPageWithBodyTimedOut(
                 @"
                 <a href='#second'>go to Heading 2</a>
@@ -26,6 +29,9 @@ namespace NSelene.Tests.Integration.SharedDriver.SeleneSpec
 
             S("a").Click();
 
+            var afterCall = DateTime.Now;
+            Assert.Greater(afterCall, beforeCall.AddSeconds(0.3));
+            Assert.Less(afterCall, beforeCall.AddSeconds(0.6));
             Assert.IsTrue(Configuration.Driver.Url.Contains("second"));
         }
 
@@ -40,6 +46,7 @@ namespace NSelene.Tests.Integration.SharedDriver.SeleneSpec
                 <h2 id='second'>Heading 2</h2>
                 "
             );
+            var beforeCall = DateTime.Now;
             Given.ExecuteScriptWithTimeout(
                 @"
                 document.getElementById('link').style.display = 'block';
@@ -49,6 +56,9 @@ namespace NSelene.Tests.Integration.SharedDriver.SeleneSpec
 
             S("a").Click();
 
+            var afterCall = DateTime.Now;
+            Assert.Greater(afterCall, beforeCall.AddSeconds(0.3));
+            Assert.Less(afterCall, beforeCall.AddSeconds(0.6));
             Assert.IsTrue(Configuration.Driver.Url.Contains("second"));
         }
 
@@ -82,6 +92,7 @@ namespace NSelene.Tests.Integration.SharedDriver.SeleneSpec
                 <h2 id='second'>Heading 2</h2>
                 "
             );
+            var beforeCall = DateTime.Now;
             Given.ExecuteScriptWithTimeout(
                 @"
                 document.getElementById('overlay').style.display = 'none';
@@ -91,7 +102,66 @@ namespace NSelene.Tests.Integration.SharedDriver.SeleneSpec
 
             S("a").Click();
 
+            var afterCall = DateTime.Now;
+            Assert.Greater(afterCall, beforeCall.AddSeconds(0.3));
+            Assert.Less(afterCall, beforeCall.AddSeconds(0.6));
             Assert.IsTrue(Configuration.Driver.Url.Contains("second"));
+        }
+
+        [Test]
+        public void Click_IsRenderedInError_OnOverlappedWithOverlayFailure()
+        {
+            Configuration.Timeout = 0.25;
+            Configuration.PollDuringWaits = 0.1;
+            Given.OpenedPageWithBody(
+                @"
+                <div 
+                    id='overlay' 
+                    style='
+                        display: block;
+                        position: fixed;
+                        display: block;
+                        width: 100%;
+                        height: 100%;
+                        top: 0;
+                        left: 0;
+                        right: 0;
+                        bottom: 0;
+                        background-color: rgba(0,0,0,0.1);
+                        z-index: 2;
+                        cursor: pointer;
+                    '
+                >
+                </div>
+
+                <a id='link' href='#second'>go to Heading 2</a>
+                <h2 id='second'>Heading 2</h2>
+                "
+            );
+            var beforeCall = DateTime.Now;
+
+            try 
+            {
+                S("a").Click();
+            }
+
+            catch (TimeoutException error)
+            {
+                var afterCall = DateTime.Now;
+                Assert.Greater(afterCall, beforeCall.AddSeconds(0.25));
+                Assert.Less(afterCall, beforeCall.AddSeconds(1.25));
+
+                var lines = error.Message.Split("\n").Select(
+                    item => item.Trim()
+                ).ToList();
+
+                Assert.Contains("Timed out after 0.25s, while waiting for:", lines);
+                Assert.Contains("Browser.Element(a).ActualWebElement.Click()", lines);
+                Assert.Contains("Reason:", lines);
+                Assert.NotNull(lines.Find(item => item.Contains(
+                    "Other element would receive the click: <div id=\"overlay\" "
+                )));
+            }
         }
     }
 }
